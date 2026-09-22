dc14ac3f02593b41a0251d08779fe65c09e30e07
lkpb-pixel <zhhcs27rkz@privaterelay.appleid.com>
2026-09-22 03:49:39 -0300

    Atualiza cache PWA para versão sincronizada

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='controle-casal-v2';
+const CACHE='controle-casal-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
